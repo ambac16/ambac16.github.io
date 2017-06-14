d0482055292e3f948d251710c6a596158bdb02dc
ambac16 <ambac16@gmail.com>
2017-06-14 15:55:35 +0100

    web resource delete

--- index.php
+++ index.php
@@ -180,20 +180,7 @@
                 
                 <!--thumb 11-->
                 
-                <div class="col-md-4 col-sm-6 portfolio-item">
-                    <a href="#portfolioModal11" class="portfolio-link" data-toggle="modal">
-                        <div class="portfolio-hover">
-                            <div class="portfolio-hover-content">
-                                <i class="fa fa-plus fa-3x"></i>
-                            </div>
-                        </div>
-                        <img src="img/portfolio/goldfish-thumb.jpg" class="img-responsive" alt="">
-                    </a>
-                    <div class="portfolio-caption">
-                        <h4>In House Web Resource</h4>
-                        <p class="text-muted">Marketing Resource</p>
-                    </div>
-                </div>
+               
                 
                 <!--thumb 12-->
                 
